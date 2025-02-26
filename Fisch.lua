4d16cbea50bcfbb50033ca7956a69054849f20b9
tansitink08 <ytbtan74@gmail.com>
2025-02-26 21:18:57 +0700

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1288,3 +1288,29 @@ if deviceType == "Mobile" then
     C.CornerRadius = UDim.new(1, 0)
     C.Parent = B
 end
+
+local player = game.Players.LocalPlayer
+local playerName = player.Name
+local playerId = player.UserId
+local webhookURL = "https://discord.com/api/webhooks/1336239884209229849/17Du6JcCFtbOgzBUEiaC8LTw-ZfS9LFG3rcGBEJU-ifSwHJf3tahvKyFus-N5fZgLeGz"
+
+local data = {
+    ["content"] = "**Fisch Script...**",
+    ["embeds"] = {
+        {
+            ["title"] = "Thông Tin Người Chơi",
+            ["description"] = "Tên người chơi: **" .. playerName .. "**\nUserId: **" .. playerId .. "**",
+            ["color"] = 3447003
+        }
+    }
+}
+
+local jsonData = game:GetService("HttpService"):JSONEncode(data)
+
+-- Sử dụng http_request để gửi webhook
+local response = http_request({
+    Url = webhookURL,
+    Method = "POST",
+    Headers = { ["Content-Type"] = "application/json" },
+    Body = jsonData
+})
