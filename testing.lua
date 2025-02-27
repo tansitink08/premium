1666de195a403e56f821a62c0ae5508bd03e457c
tansitink08 <ytbtan74@gmail.com>
2025-02-27 22:08:57 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -52,14 +52,21 @@ Frame.Size = UDim2.new(0, 400, 0, 300)
 Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
 Frame.AnchorPoint = Vector2.new(0.5, 0.5)
 Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+Frame.BorderSizePixel = 0
 Frame.Active = true
 Frame.Draggable = true
 Frame.Parent = ScreenGui
 
+local FrameCorner = Instance.new("UICorner")
+FrameCorner.CornerRadius = UDim.new(0, 10)
+FrameCorner.Parent = Frame
+
 local Close = Instance.new("TextButton")
 Close.Size = UDim2.new(0, 40, 0, 40)
 Close.Position = UDim2.new(1, -40, 0, 0)
+Close.BackgroundTransparency = 1
 Close.Text = "×"
+Close.TextScaled = true
 Close.TextColor3 = Color3.fromRGB(150, 150, 150)
 Close.Parent = Frame
 Close.MouseButton1Click:Connect(function()
@@ -69,7 +76,8 @@ end)
 local Title = Instance.new("TextLabel")
 Title.Size = UDim2.new(1, 0, 0, 30)
 Title.Position = UDim2.new(0, 0, 0.05, 0)
-Title.Text = "Dino Key System🔑"
+Title.Text = "Key System"
+Title.TextSize = 18
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
 Title.BackgroundTransparency = 1
 Title.Parent = Frame
@@ -80,23 +88,39 @@ TextBox.Position = UDim2.new(0.1, 0, 0.4, 0)
 TextBox.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 TextBox.PlaceholderText = "Enter Key..."
 TextBox.Text = ""
+TextBox.TextSize = 18
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.TextScaled = true
 TextBox.Parent = Frame
+
+local TextBoxCorner = Instance.new("UICorner")
+TextBoxCorner.CornerRadius = UDim.new(0, 5)
+TextBoxCorner.Parent = TextBox
 
 local GetKey = Instance.new("TextButton")
 GetKey.Size = UDim2.new(0.35, 0, 0.15, 0)
 GetKey.Position = UDim2.new(0.1, 0, 0.7, 0)
+GetKey.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 GetKey.Text = "Get Key"
+GetKey.TextSize = 18
 GetKey.TextColor3 = Color3.fromRGB(150, 150, 150)
 GetKey.Parent = Frame
+
+local GetKeyCorner = Instance.new("UICorner")
+GetKeyCorner.CornerRadius = UDim.new(0, 5)
+GetKeyCorner.Parent = GetKey
 
 local CheckKey = Instance.new("TextButton")
 CheckKey.Size = UDim2.new(0.35, 0, 0.15, 0)
 CheckKey.Position = UDim2.new(0.55, 0, 0.7, 0)
+CheckKey.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 CheckKey.Text = "Check Key"
+CheckKey.TextSize = 18
 CheckKey.TextColor3 = Color3.fromRGB(150, 150, 150)
 CheckKey.Parent = Frame
+
+local CheckKeyCorner = Instance.new("UICorner")
+CheckKeyCorner.CornerRadius = UDim.new(0, 5)
+CheckKeyCorner.Parent = CheckKey
 
 -- Thêm phần setclipboard vào nút "Get Key"
 GetKey.MouseButton1Click:Connect(function()
