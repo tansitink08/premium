8cf5fede23ad8704718663425c03aa5a00e9c1d2
tansitink08 <ytbtan74@gmail.com>
2025-02-27 21:38:55 +0700

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -1,21 +1,3 @@
-local validKeys = {
-    "dino123",
-    "Key2",
-    "Key3",
-}
-
-local function isValidKey(inputKey)
-    for _, key in ipairs(validKeys) do
-        if inputKey == key then
-            return true
-        end
-    end
-    return false
-end
-
-if _G.key then
-    if isValidKey(_G.key) then
-        print("Key hợp lệ! Đang tải script...")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
@@ -9466,9 +9448,3 @@ local response = http_request({
     Headers = { ["Content-Type"] = "application/json" },
     Body = jsonData
 })
-    else
-        print("Key không hợp lệ! Kết thúc chương trình.")
-    end
-else
-    print("Không có key được nhập. Chương trình không thực hiện gì.")
-end
