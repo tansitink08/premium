3b885575d144aed021e6a5cf91f9aaecf9dab70e
tansitink08 <ytbtan74@gmail.com>
2025-03-02 22:01:39 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -18,7 +18,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 340),
     Acrylic = false,
-    Theme = "Dark",
+    Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
