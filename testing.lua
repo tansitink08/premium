05ec87ca4cda6e47e1b0a9f99a26b6f0429db2b9
tansitink08 <ytbtan74@gmail.com>
2025-03-26 20:30:25 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -3,7 +3,7 @@ local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"994304177
       local General_Tab = Window:AddTab("General",[[7040391851]])
       local Quest_Tab = Window:AddTab("Dont care",[[9606626859]])
 
-    General_Tab:Label("Thx for use!")
+    General_Tab:Label("Premium script | Thx for use!")
 
    General_Tab:Button("OneClick",function()
    loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
