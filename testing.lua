e7360bbaea03117d3d609fbbf9c0e8a297dfe0db
tansitink08 <ytbtan74@gmail.com>
2025-03-26 19:46:55 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -3,28 +3,10 @@ local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"994304177
       local General_Tab = Window:AddTab("General",[[7040391851]])
      local Quest_Tab = Window:AddTab("Quest & Item",[[9606626859]])
 
-    General_Tab:Label("Label Tile")
-  
-   General_Tab:Toggle("Toggle","9606294253",false,function()
-  end)
+    General_Tab:Label("Thx for use!")
 
-   General_Tab:Button("Button",function()
+   General_Tab:Button("OneClick",function()
    end)
-   
-   AutoSet = {
-    "Set 1",
-    "Set 2",
-    "Set 3",
-    "Set 4"
-}
-General_Tab:Dropdown("Dropdown Test",AutoSet,function()
-end)
-    
-    local Tessss = General_Tab:LabelP("Check")
-    Tessss:Set("Test")
-    
-General_Tab:Slider("Slider",1,100,60,function()
-end)    
-    
-  General_Tab:Textbox("TextBox",function()  
+  
+   General_Tab:Toggle("Black screen","9606294253",false,function()
   end)
