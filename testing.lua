11ea56d0c6491e0e5c75e8661deb4364222f08c2
tansitink08 <ytbtan74@gmail.com>
2025-03-26 19:38:25 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,5 +1,5 @@
  local dino = loadstring(game:HttpGet"https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/fixedui.lua")()
-local Window = library:Window("Switch",[[Hub]],[[Version : OneClick  ]],"99430417788026",Enum.KeyCode.RightControl)
+local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"99430417788026",Enum.KeyCode.RightControl)
       local General_Tab = Window:AddTab("General",[[7040391851]])
      local Quest_Tab = Window:AddTab("Quest & Item",[[9606626859]])
 
