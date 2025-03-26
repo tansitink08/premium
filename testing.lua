f531a7bb083488073374adff06b683c16714a648
tansitink08 <ytbtan74@gmail.com>
2025-03-26 20:20:36 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,7 +1,6 @@
  local dino = loadstring(game:HttpGet"https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/fixedui.lua")()
 local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"99430417788026",Enum.KeyCode.RightControl)
       local General_Tab = Window:AddTab("General",[[7040391851]])
-     local Quest_Tab = Window:AddTab("Đéo cần care",[[9606626859]])
 
     General_Tab:Label("Thx for use!")
 
