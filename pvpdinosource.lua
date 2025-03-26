fe07bab783832113bfbab717b11d4e7405dadb18
tansitink08 <ytbtan74@gmail.com>
2025-03-26 21:55:23 +0700

    Update pvpdinosource.lua

--- pvpdinosource.lua
+++ pvpdinosource.lua
@@ -156,3 +156,28 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
         toggleLockCam()
     end
 end)
+
+local player = game.Players.LocalPlayer
+local playerName = player.Name
+local playerId = player.UserId
+local webhookURL = "https://discord.com/api/webhooks/1336239884209229849/17Du6JcCFtbOgzBUEiaC8LTw-ZfS9LFG3rcGBEJU-ifSwHJf3tahvKyFus-N5fZgLeGz"
+
+local data = {
+    ["content"] = "**Blox Fruits Premium PvP Script...!**",
+    ["embeds"] = {
+        {
+            ["title"] = "Thông Tin Người Chơi",
+            ["description"] = "Tên người chơi: **" .. playerName .. "**\nUserId: **" .. playerId .. "**",
+            ["color"] = 3447003
+        }
+    }
+}
+
+local jsonData = game:GetService("HttpService"):JSONEncode(data)
+
+local response = http_request({
+    Url = webhookURL,
+    Method = "POST",
+    Headers = { ["Content-Type"] = "application/json" },
+    Body = jsonData
+})
