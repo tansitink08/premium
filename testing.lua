0bf694cc478749070bc4cd8af0f831171294f7c4
tansitink08 <ytbtan74@gmail.com>
2025-03-26 21:37:03 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -3,7 +3,7 @@ local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"994304177
       local General_Tab = Window:AddTab("General",[[7040391851]])
       local Quest_Tab = Window:AddTab("Dont care",[[9606626859]])
 
-    General_Tab:Label("Premium script | Thx for use!")
+    General_Tab:Label("spawn(function() while true do Timmessss:SetDesc(math.floor(workspace.DistributedGameTime/3600)%24..' Hour (h) '..math.floor(workspace.DistributedGameTime/60)%60..' Minute (m) '..math.floor(workspace.DistributedGameTime)%60..' Second (s) ') wait(1) end end)")
 
    General_Tab:Button("OneClick",function()
    loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
