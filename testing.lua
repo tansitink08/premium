14cf6036e89d2a8bee4d87980e235365e267054f
tansitink08 <ytbtan74@gmail.com>
2025-03-26 20:28:13 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,10 +1,16 @@
  local dino = loadstring(game:HttpGet"https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/fixedui.lua")()
 local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"99430417788026",Enum.KeyCode.RightControl)
       local General_Tab = Window:AddTab("General",[[7040391851]])
+      local Quest_Tab = Window:AddTab("Quest & Item",[[9606626859]])
 
     General_Tab:Label("Thx for use!")
 
    General_Tab:Button("OneClick",function()
    loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
    end)
-  
+
+   General_Tab:Toggle("Toggle","9606294253",false,function()
+  end)
+
+   General_Tab:Toggle("Boost FPS","9606294253",false,function()
+  end)
