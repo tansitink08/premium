042ca9e96a36c8b899a1c1aedc3c24b853fd2b16
tansitink08 <ytbtan74@gmail.com>
2025-03-26 20:15:13 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -9,26 +9,52 @@ local Window = library:Window("Dino",[[Hub]],[[Version : OneClick  ]],"994304177
    loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
    end)
   
-General_Tab:Toggle("Black screen", "9606294253", false, function(state)
-    getgenv().StartBlackScreen = state
-end)
-
-local lastUpdateTime = 0
-local updateCooldown = 0.5
-
-RunService.RenderStepped:Connect(function()
-    if tick() - lastUpdateTime >= updateCooldown then
-        lastUpdateTime = tick()
-        local blackScreen = LocalPlayer.PlayerGui:FindFirstChild("Main") and LocalPlayer.PlayerGui.Main:FindFirstChild("Blackscreen")
-        if blackScreen then
-            blackScreen.Size = getgenv().StartBlackScreen and UDim2.new(1, 0, 1, 0) or UDim2.new(0, 0, 0, 0)
+   General_Tab:Button("Boost FPS",function()
+ function FPSBooster()
+    local decalsyeeted = true
+    local g = game
+    local w = g.Workspace
+    local l = g.Lighting
+    local t = w.Terrain    
+    sethiddenproperty(l, "Technology", 2)
+    sethiddenproperty(t, "Decoration", false)
+    t.WaterWaveSize = 0
+    t.WaterWaveSpeed = 0
+    t.WaterReflectance = 0
+    t.WaterTransparency = 0
+    l.GlobalShadows = false
+    l.FogEnd = 9e9
+    l.Brightness = 0
+    settings().Rendering.QualityLevel = "Level01"    
+    local function optimizePart(v)
+        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+        elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+            v.Transparency = 1
+        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+            v.Lifetime = NumberRange.new(0)
+        elseif v:IsA("Explosion") then
+            v.BlastPressure = 1
+            v.BlastRadius = 1
+        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+            v.Enabled = false
+        elseif v:IsA("MeshPart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+            v.TextureID = 10385902758728957
+        end
+    end    
+    for i, v in pairs(w:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") or v:IsA("MeshPart") then
+            optimizePart(v)
         end
     end
-end)
-
-General_Tab:Toggle("White screen", "9606294253", false, function(state)
-    getgenv().WhiteScreen = state
-    RunService:Set3dRenderingEnabled(not state)
+    for i, e in pairs(l:GetChildren()) do
+        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+            e.Enabled = false
+        end
+    end
 end)
 
 
