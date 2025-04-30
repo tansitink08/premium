6a8a0eb0381159f911d77417409551639e59f8ef
tansitink08 <ytbtan74@gmail.com>
2025-04-30 13:00:10 +0700

    Update paidxhub.lua

--- paidxhub.lua
+++ paidxhub.lua
@@ -1,7 +1,8 @@
 local player = game.Players.LocalPlayer
 
 local premiumKeys = {
-    "DINO-PJK64-F5Z8-2020-V4I24-88PK8"
+    "DINO-PJK64-F5Z8-2020-V4I24-88PK8",
+    "DINO-3D96J-FKJJ-L98W-74233-KDV88"
 }
 
 local function isPremiumKey(inputKey)
@@ -13,7 +14,7 @@ local function isPremiumKey(inputKey)
     return false
 end
 
-if _G.mode == "MaruMobile" then
+if _G.mode == "Marumb08" then
     if not _G.key or _G.key == "" or not isPremiumKey(_G.key) then
         local notifi = "Premium Only | _G.key = \"put ur key\""
         local message = string.format("\nPremium Announcement\n %s", notifi)
@@ -24,3 +25,13 @@ if _G.mode == "MaruMobile" then
     return 
 end  
 
+if _G.mode == "Marupc02" then
+    if not _G.key or _G.key == "" or not isPremiumKey(_G.key) then
+        local notifi = "Premium Only | _G.key = \"put ur key\""
+        local message = string.format("\nPremium Announcement\n %s", notifi)
+        player:Kick(message)
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tansitink08/premium/refs/heads/main/paidpc.lua"))()
+    end
+    return 
+end  
